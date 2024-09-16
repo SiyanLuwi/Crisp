6d9e5cbbdd21ad51e827c7dcd708c6789b378d88
Sean Lowie <sean.bearbone@gmail.com>
2024-09-16 13:54:07 +0800

    functional camera capture

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,22 +1,24 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text } from "react-native";
 
 export default function Profile() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}> Profile Will be Here </Text>
+      <View>
+        <Text style={styles.text}> Profile </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#0C3B2D',
-      justifyContent: 'center',
-      alignItems: 'center',
-
-    },
-    text: {
-      color: '#ffffff', 
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#0C3B2D",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#ffffff",
+  },
+});
